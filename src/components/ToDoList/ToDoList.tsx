8a0ee64425384608cfa8433bfd034ff5a3f56d09
Igor Borozdov <melnikow_igor@mail.ru>
2022-03-25 20:39:25 +0600

    Eslint fix

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -5,14 +5,21 @@ import ToDo from '../../types/interfaces'
 
 const ToDoList: React.FC = () => {
     const ToDo: Array<ToDo> = [
-        {id: 0, task: "ToDo1", completed: false},
-        {id: 1, task: "ToDo2", completed: false},
-        {id: 2, task: "ToDo3", completed: false},
+        { id: 0, task: 'ToDo1', completed: false },
+        { id: 1, task: 'ToDo2', completed: false },
+        { id: 2, task: 'ToDo3', completed: false },
     ]
 
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: '#f5f5f5' }}>
-            {ToDo.map(item => <ToDoItem key={item.id} id={item.id} task={item.task} completed={item.completed} />)}
+            {ToDo.map(item => (
+                <ToDoItem
+                    key={item.id}
+                    id={item.id}
+                    task={item.task}
+                    completed={item.completed}
+                />
+            ))}
         </List>
     )
 }
